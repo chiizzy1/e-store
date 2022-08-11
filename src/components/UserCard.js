47ff8fdaa2461cbc61e2749d776524e1226069fc
Chinwuba Israel <chiizzy5@gmail.com>
2022-08-11 19:04:17 +0100

    customer testimonial updated

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function UserCard({firstName, lastName, city, img}) {
-    console.log(img);
+    // console.log(img);
   return (
-    <div>UserCard</div>
+    <Card>
+        <p>Lorem ipsum, dolor sit ratione.</p>
+        <Detail>
+            <Image src={img} />
+            <div>
+                <h4>{`${firstName} ${lastName}`}</h4>
+                <p>{city}</p>
+            </div>
+        </Detail>
+    </Card>
   )
 }
 
 export default UserCard
+
+const Card = styled.div`
+    padding: 1em;
+`
+
+const Detail = styled.div`
+    display: flex;
+
+    div{
+        display: flex;
+        flex-direction: column;
+    }
+`
+
+const Image = styled.img`
+    height: 4em;
+    width: 4em;
+    border-radius: 50%;
+`
