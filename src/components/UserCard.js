929d0d7a6604454768777780edd54ddb0dda11f1
Chinwuba Israel <chiizzy5@gmail.com>
2022-08-16 20:56:35 +0100

    website update

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -5,14 +5,18 @@ function UserCard({firstName, lastName, city, img}) {
     // console.log(img);
   return (
     <Card>
-        <p>Lorem ipsum, dolor sit ratione.</p>
-        <Detail>
-            <Image src={img} />
-            <div>
-                <h4>{`${firstName} ${lastName}`}</h4>
-                <p>{city}</p>
-            </div>
-        </Detail>
+        <Padding>
+
+            <p>Lorem ipsum, dolor sit ratione.</p>
+            <Detail>
+                <Image src={img} />
+                <div>
+                    <h4>{`${firstName} ${lastName}`}</h4>
+                    <p>{city}</p>
+                </div>
+            </Detail>
+
+        </Padding>
     </Card>
   )
 }
@@ -20,15 +24,33 @@ function UserCard({firstName, lastName, city, img}) {
 export default UserCard
 
 const Card = styled.div`
-    padding: 1em;
+    background: #222;
+    color: #fff;
+    border: 1px solid red;
+    font-size: 1.1rem;
+`
+
+const Padding = styled.div`
+    padding: 2em;
+    p{
+        // line-height: 2px;
+        // letter-spacing: 1px;
+        // padding: 2em 0;
+    }
 `
 
 const Detail = styled.div`
     display: flex;
+    // justify-content: space-between;
+    padding-top: 1.5em;
 
     div{
         display: flex;
         flex-direction: column;
+
+        p{
+            padding-top: 1em;
+        }
     }
 `
 
@@ -36,4 +58,5 @@ const Image = styled.img`
     height: 4em;
     width: 4em;
     border-radius: 50%;
+    margin-right: 1.5em;
 `
